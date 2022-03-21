51b4b531fbecdf59062cc2b7a527573217fa61f4
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-03-21 01:52:18 -0700

    complete weather app

---
+++ scripts/forecast.js
@@ -0,0 +1,36 @@
+const key = "OypRZ9oGYZCiskC5Gb7fQFwtZhFYVwMt";
+
+//get weather info
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  // console.log(data[0]);
+  return data[0];
+};
+
+//get city info
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  //   console.log(data[0]);
+  return data[0];
+};
+
+// getCity("manchester")
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
+
+// getWeather("329260");
